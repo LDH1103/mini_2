3cc77dd1924a0e04a07c6cbd34accf7ba209985b
LDH1103 <ldh517525@gmail.com>
2023-05-14 15:27:48 +0900

    23-05-14

--- application/view/list.php
+++ application/view/list.php
@@ -8,15 +8,8 @@
 </head>
 <body>
     List File!!!!
-    <!-- <button id="logout" onclick="logoutBtn();">Logout</button> -->
     <button id="logout">Logout</button>
 
-
     <script src="/application/view/js/list.js"></script>
-    <!-- <script>
-        function logoutBtn() {
-            location.href = "/user/logout";
-        }
-    </script> -->
 </body>
 </html>
